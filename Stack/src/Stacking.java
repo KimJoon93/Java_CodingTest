3c72eafeedf22cff6fa9a05651ecd68d297d1b6a
Kim Joon <jk1094@naver.com>
2019-09-21 18:55:47 +0900

    Stack poping

---
+++ Stack/src/Stacking.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Stacking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//아래부터 쌓고 위에서 부터 꺼냄
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(5);
+		stack.push(4);
+		stack.push(3);
+		stack.push(2);
+		stack.push(1);
+		
+		System.out.println(stack);
+		System.out.println("Stack 사이즈: "+stack.size());
+		System.out.println("Stack 가장 위: "+ stack.peek());
+		System.out.println("Stack re사이즈: "+stack.size());
+		
+		System.out.println(stack.pop());
+		
+		System.out.println("Stack 1개 뽑은 후 사이즈 : "+stack.size());
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		
+		//오류 발생 EmptyStack Exception
+		System.out.println(stack.pop());
+		
+		
+		
+
+	}
+
+}
