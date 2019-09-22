8c46b33e3ba05bb573130b8cdf6b65b8d9b7623c
Kim Joon <jk1094@naver.com>
2019-09-22 23:50:32 +0900

    Factorial

---
+++ Factorial/src/factorial.java
@@ -0,0 +1,15 @@
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 19;
+		int result = 1;
+        for(int i = 1; i<=n;i++) {
+            result *= i;
+
+        }
+        System.out.println(result);
+	}
+
+}
