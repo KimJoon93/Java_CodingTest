c1c3909d688d4e44d4ebae4b0f1b3414403123c3
Kim Joon <jk1094@naver.com>
2019-09-24 21:53:15 +0900

    operator

---
+++ Algorithm_#1/src/Operator.java
@@ -0,0 +1,17 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 1;
+		int c = 0;
+		
+		int a = (x>y) ? x : y;
+		
+		System.out.println(a);
+		
+		System.out.println((c==0)? "c는 0" : "c는 0아님");
+	}	
+
+}
