f0b8eef015ee52a05fb2234c595fecc769110051
Kim Joon <jk1094@naver.com>
2019-09-24 23:32:20 +0900

    SumFor

---
+++ Algorithm_#1/src/SumFor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1부터 n까지의 합을 구한다! ");
+		System.out.print("n의 값: ");
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		
+		int sum2 = 0;
+		
+		sum2= n*(n+1)/2;
+		
+		for(int i=0; i<=n; i++)
+			sum+=i;
+		
+		System.out.println(sum);
+		
+		System.out.println("sum2의 값: " + sum2);
+	}
+
+}
