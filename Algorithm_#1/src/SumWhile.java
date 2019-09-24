490ec02f5fb6a7a8be6b574f153b7c9d5c3f110e
Kim Joon <jk1094@naver.com>
2019-09-24 22:23:11 +0900

    WhileSum Check i

--- Algorithm_#1/src/SumWhile.java
+++ Algorithm_#1/src/SumWhile.java
@@ -8,18 +8,23 @@ public class SumWhile {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("n의 값: ");
+		
 		int n = sc.nextInt();
 		
 		int sum = 0;
+		
 		int i = 1;
 		
 		while(i<=n) {
+			
 			sum += i;
 			i++;
+			System.out.println("i 값: " + i);
+			
 		}
 		
-		
-		System.out.print("Sum 값: " + sum);
+		System.out.println("Sum 값: " + sum);
+		System.out.println("i 값: " + i);
 	}
 
 }
