dd71419cd698b420d39b6a9d3518efa8493468e7
Kim Joon <jk1094@naver.com>
2019-09-24 18:18:58 +0900

    Max num

--- Algorithm_#1/src/Start.java
+++ Algorithm_#1/src/Start.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("세 정수의 최대값을 구합니다!");
+		
+		System.out.print("a의 값: ");
+		int a = sc.nextInt();
+		
+		System.out.print("b의 값: ");
+		int b = sc.nextInt();
+		
+		System.out.print("c의 값: ");
+		int c = sc.nextInt();
+		
+		
+		int max = a;
+		
+		if(b>max)
+			max=b;
+		if(c>max)
+			max=c;
+		
+		System.out.println("최대값은 "+ max + "입니다. ");
 	}
 
 }
