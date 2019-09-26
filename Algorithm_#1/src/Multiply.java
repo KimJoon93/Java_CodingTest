c35969cc474fb4efe03762a289420e322b5247c6
Kim Joon <jk1094@naver.com>
2019-09-26 23:31:20 +0900

    Board

--- Algorithm_#1/src/Multiply.java
+++ Algorithm_#1/src/Multiply.java
@@ -11,12 +11,22 @@ public class Multiply {
 		
 		int n = 5;
 		
+		int k = 1;
+		
+		int nums = 0;
+		
 		for(int j=1; j<n+1; j++) {
 			
-			la.add(j*(j+1));
-			la.add(j*(j+1)*(j+2));
-			la.add(j*(j+1)*(j+2)*(j+3));
-			la.add(j*(j+1)*(j+2)*(j+3)*(j+4));
+			if(nums <= n*(n+1)) {
+				nums = j*(j+k);
+				la.add(nums);
+				
+				
+//				k++;
+//				nums = nums * (j+k);
+//				la.add(nums);
+			}
+			
 		}
 		
 		Collections.sort(la);
@@ -32,17 +42,8 @@ public class Multiply {
 			 }
 		 }
 
-		System.out.println(realla.get(n));
-		
-//		int j = 0;
-//		
-//		int a = j;
-//		
-//		for(int i=0; i<10; i++) {
-//			a *= j+i;
-//			
-//			System.out.println(a);
-//		}
+		System.out.println(realla.get(n-1));
+		System.out.println(realla);
 	}
 
 }
