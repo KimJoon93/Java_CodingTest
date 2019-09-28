f55e46f04673589aa26efbe59d3b4d4c0c5d9b71
Kim Joon <jk1094@naver.com>
2019-09-28 15:48:48 +0900

    Array

---
+++ Algorithm_#2/src/Array.java
@@ -0,0 +1,18 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[5];
+		
+		a[1] = 37;
+		a[2] = 32;
+		a[4] = a[1] * 2;
+		
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
