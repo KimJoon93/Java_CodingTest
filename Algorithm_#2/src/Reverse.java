281e36b8a2fe146380060e621688d168329edf67
Kim Joon <jk1094@naver.com>
2019-09-28 16:40:24 +0900

    Swap

--- Algorithm_#2/src/Reverse.java
+++ Algorithm_#2/src/Reverse.java
@@ -7,17 +7,28 @@ public class Reverse {
 		
 		int n = array.length;
 		
-		for(int i=0; i<n/2; i++) {
-			int memory = array[i];
-			array[i] = array[n-i-1];
-			
-			array[n-i-1] = memory;
-		}
+//		for(int i=0; i<n/2; i++) {
+//			int memory = array[i];
+//			array[i] = array[n-i-1];
+//			
+//			array[n-i-1] = memory;
+//		}
 		
 		
+		
+		
+		
+		swap(array,1,5);
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
 		}
+	}
+	
+	
+	static void swap(int[] a , int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
 	}
 
 }
